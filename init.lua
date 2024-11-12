07574fdfeec76b896f4ea2eff938f0d464cb83bd
Michael O'Rawe <michaelorawe@btinternet.com>
2024-11-12 22:21:15 +0000

    neovim config

---
+++ init.lua
@@ -0,0 +1,65 @@
+-- Tab settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- Visual settings
+vim.cmd("set rnu")
+vim.cmd("set mouse=")
+
+-- Import lazy nvim config
+require("config.lazy")
+
+-- Telescope setup
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-l>", builtin.live_grep, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+require("telescope").setup({
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({}),
+		},
+	},
+})
+require("telescope").load_extension("ui-select")
+
+-- Set catppuccin
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
+
+-- Treesitter config
+local config = require("nvim-treesitter.configs")
+config.setup({
+	-- ensure_installed = { "c", "python", "lua" },
+	auto_install = true,
+	highlight = { enable = true },
+	indent = { enable = true },
+})
+
+-- Lualine config
+require("lualine").setup({
+	options = { theme = "OceanicNext" },
+})
+
+-- Mason install for lsp
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "clangd", "lua_ls" },
+})
+require("lspconfig").lua_ls.setup({})
+require("lspconfig").clangd.setup({})
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+-- None-ls
+local null_ls = require("null-ls")
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.cppcheck,
+		null_ls.builtins.formatting.clang_format,
+	},
+})
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
---
+++ lua/plugins.lua
@@ -0,0 +1,17 @@
+return {
+  -- plugins
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "neovim/nvim-lspconfig" },
+    { "nvimtools/none-ls.nvim" }
+}
